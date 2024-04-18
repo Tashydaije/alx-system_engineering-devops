d93052b5806e25bd275178d92d5daeeabb08ad6a
Tashydaije <mbuguanatasha01@gmail.com>
2024-04-18 13:38:48 -0700

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,20 +1,18 @@
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    hasrestart => true,
-  }
-}
+# Puppet script ot increase nginx 
 
 $nginx_ulimit = 'ULIMIT="-n 65535"'
-class { 'nginx': }
+
+package { 'nginx':
+}
 
 file { '/etc/default/nginx':
   ensure  => present,
   content => $nginx_ulimit,
-  notify  => Service['nginx'],
+  notify  => Service['nginx']
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
 }
