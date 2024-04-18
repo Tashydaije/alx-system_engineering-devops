a8316c2489c60cde287728a114788ce45e72bc3c
Tashydaije <mbuguanatasha01@gmail.com>
2024-04-18 09:15:37 -0700

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,18 +1,20 @@
-$nginx_ulimit = 'ULIMIT="-n 70000"'
-# Define Nginx class
-class { 'nginx':
-  ensure  => 'installed',
-  service => 'running'
+class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    hasrestart => true,
+  }
 }
+
+$nginx_ulimit = 'ULIMIT="-n 65535"'
+class { 'nginx': }
 
 file { '/etc/default/nginx':
   ensure  => present,
-  content => $nginx_ulimit
-  notify  => Service['nginx']
-}
-
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
+  content => $nginx_ulimit,
+  notify  => Service['nginx'],
 }
